629697b7986b3597f93fb9eac491e4e490d37421
Author55 <shreyansh.anshuman22@gmail.com>
2018-01-13 23:01:24 +0530

    AimAt

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -18,6 +18,21 @@ void ATankAIController::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("AIController Begin Play"));
 }
 
+void ATankAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (GetPlayerTank())
+	{
+		// TODO Move towards the player
+
+		//Aim towards the player
+		GetControlledTank()->AimAt(GetPlayerTank()->GetActorLocation());
+
+		//Fire if ready
+
+	}
+}
+
 ATank* ATankAIController::GetControlledTank() const
 {
 
@@ -31,4 +46,5 @@ ATank * ATankAIController::GetPlayerTank() const
 	
 	return Cast<ATank>(PlayerPawn);
 }
+
 
