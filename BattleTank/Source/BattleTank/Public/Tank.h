721f55b713a823e5fa11d6810749770f58a96e39
Author55 <shreyansh.anshuman22@gmail.com>
2018-01-14 22:43:15 +0530

    UGameplayStatics::SuggestProjectileVelocity

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -3,8 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h" ///Put new includes above
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -17,6 +18,9 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
 public:	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -24,4 +28,11 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	
 	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000; // TODO find sensible default
+
 };
---
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -0,0 +1,32 @@
+// (c) Shreyansh Anshuman
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "TankAimingComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API UTankAimingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+private:	
+	// Sets default values for this component's properties
+	UTankAimingComponent();
+	UStaticMeshComponent* Barrel = nullptr;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+	void AimAt(FVector HitLocation, float LaunchSpeed);
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+	
+};
