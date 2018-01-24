e24899d7189548f9302c4acf6f8d9bcc6e8e9548
Author55 <shreyansh.anshuman22@gmail.com>
2018-01-25 00:40:00 +0530

    GREEN!!

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -11,15 +11,14 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-	UE_LOG(LogTemp, Warning, TEXT("DONKEY: Constructor"))
-
 }
 
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();   // Needed for BP Begin Play to run!
-	UE_LOG(LogTemp, Warning, TEXT("DONKEY: Begin Play"))
+
+	TankAimingComponent = FindComponentByClass<UTankAimingComponent>();
 }
 
 void ATank::AimAt(FVector HitLocation)
