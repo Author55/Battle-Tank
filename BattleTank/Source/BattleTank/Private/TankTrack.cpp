616130cf588b650c91136e0a78e3a49b2234e062
Author55 <shreyansh.anshuman22@gmail.com>
2018-01-26 17:49:14 +0530

    Tank Track Tick (Initial)

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,6 +2,16 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	UE_LOG(LogTemp, Warning, TEXT("Tick Running"))
+}
 
 void UTankTrack::SetThrottle(float Throttle)
 {
